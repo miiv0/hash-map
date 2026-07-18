0fa47cbe0580ee1ddfc4b37bf3ae5e732dfc9267
miiv0 <minzana2002@gmail.com>
2026-07-18 12:37:20 -0400

    add hash and constructor

---
+++ index.js
@@ -0,0 +1,20 @@
+class hashMap {
+
+    constructor(size = 50) {
+        this.buckets = new Array(size);
+        this.size = size
+    }
+
+    hash(key) {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        }
+
+        return hashCode;
+    }
+
+
+}   

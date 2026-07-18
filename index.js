bda412d1848b3c660b15d1543365b4626b78cfbc
Michael Inzana <84941325+miiv0@users.noreply.github.com>
2026-07-18 14:43:45 -0400

    Update index.js
    Add check index

--- index.js
+++ index.js
@@ -11,15 +11,23 @@ class hashMap {
         const primeNumber = 31;
         for (let i = 0; i < key.length; i++) {
             hashCode = primeNumber * hashCode + key.charCodeAt(i);
+            hashCode = hashCode % buckets.length
         }
 
         return hashCode;
     }
+    
+    checkIndex(key) {
+        if (index < 0 || index >= buckets.length) {
+            throw new Error("Trying to access index out of bounds");
+        }
+    }
+
 
     set(key, value) {
         let index = this.hash(key)
         console.log(key)
-
+        
         if (!this.buckets[index]) {
             this.buckets[index] = []
 
